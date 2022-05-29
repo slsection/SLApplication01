eecd7ee0b9c463b12d8b7859806ce2b021fc2b01
kawashima <t-kawashima@media-bahn.com>
2022-05-29 22:32:44 +0900

    録音ファイル転送処理追加

--- app/src/main/java/com/example/slapplication01/MainActivity.java
+++ app/src/main/java/com/example/slapplication01/MainActivity.java
@@ -16,6 +16,16 @@ import java.io.IOException;
 import java.util.Locale;
 import android.media.MediaRecorder;
 
+//AWS_S3アップロード関連で必要なライブラリ
+import android.util.Log;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final long START_TIME = 180000;
@@ -57,26 +67,33 @@ public class MainActivity extends AppCompatActivity {
 
         recorder = new MediaRecorder();
 
-
+        // クリックイベント：開始 or 一時停止
         mButtonStartPause.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 System.out.println("mTimerRunningの値は？ " +mTimerRunning);
                 if (mTimerRunning) {
+                    // タイマーが動いてたら
+                    // 録音(一時)停止
                     recorder.stop();
                     recorder.release();
                     pauseTimer();
                 } else {
+                    // タイマーが動いてなかったら
+                    // startTimer()の中で録音開始
                     startTimer();
                 }
             }
         });
 
+        // クリックイベント：停止
         getmButtonFinish.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 System.out.println("ストップボタン" +mTimerRunning);
                 if (mTimerRunning) {
+                    // タイマーが動いてたら
+                    // 録音停止
                     recorder.stop();
                     recorder.release();
                     mCountDownTimer.cancel();
@@ -87,12 +104,16 @@ public class MainActivity extends AppCompatActivity {
                     mButtonStartPause.setVisibility(View.VISIBLE);
                     getmButtonReset.setVisibility(View.INVISIBLE);
                     getmButtonFinish.setVisibility(View.INVISIBLE);
+                    // S3に録音ファイルを転送
+                    uploadToS3(FILENAME, audiofile.getAbsolutePath());
                 } else {
-
+                    // タイマーが動いてなかったら
+                    // ・・・
                 }
             }
         });
 
+        // クリックイベント：リセット
         getmButtonReset.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -115,11 +136,13 @@ public class MainActivity extends AppCompatActivity {
                 recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
                 recorder.setOutputFile(audiofile.getAbsolutePath());
                 System.out.println("パス " +audiofile.getAbsolutePath());
+                // 録音準備
                 try {
                     recorder.prepare();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                // 録音開始
                 recorder.start();
 
                 getmButtonReset.setVisibility(View.VISIBLE);
@@ -161,5 +184,45 @@ public class MainActivity extends AppCompatActivity {
         int seconds = (int)(mTimeLeftInMillis/1000)%60;
         String timerLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
         mTextViewCountDown.setText(timerLeftFormatted);
+    }
+
+    // 録音ファイル転送処理
+    private void uploadToS3(String p_fileName, String p_path){
+        // S3関連の設定
+        String accessKey = "AKIAZ4U7UPY4QXCTUVVB";                  // アクセスキー
+        String secKey = "4UwlXpmzNdCy80/tvtuIp45GJIBY2SRcdu0bjN6g"; // シークレットキー
+        String bucket = "slaplication01-in";                        // バケット名
+
+        // 認証情報の作成
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secKey);
+
+        // 作成した認証情報でクライアント接続用オブジェクトを作成
+        AmazonS3Client s3Client = new AmazonS3Client(basicAWSCredentials);
+        TransferUtility transferUtility = new TransferUtility(s3Client, getApplicationContext());
+
+        // ファイルを指定してアップロードを行う
+        TransferObserver observer = transferUtility.upload(
+                bucket,
+                p_fileName,
+                new java.io.File(p_path));
+
+        // コールバック登録
+        observer.setTransferListener(new TransferListener() {
+            // 完了時
+            @Override
+            public void onStateChanged(int id, TransferState state) {
+                Log.d("uploadToS3", "status: " + state);
+            }
+            // 転送中
+            @Override
+            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+                Log.d("uploadToS3", "progress: "+id+" bytesCurrent:"+bytesCurrent+" bytesTotal:"+bytesTotal);
+            }
+            // 異常終了
+            @Override
+            public void onError(int id, Exception ex) {
+                ex.printStackTrace();
+            }
+        });
     }
 }
